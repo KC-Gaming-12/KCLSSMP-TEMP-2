2ad995e005bb897cee1e4877238d7ad9115473a2
Tony <kcgaming@lifesteal.team>
2024-11-21 08:56:31 -0700

    Update AboutSectionOne.tsx

--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -24,8 +24,8 @@ const AboutSectionOne = () => {
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 lg:w-1/2">
               <SectionTitle
-                title="Crafted for Startup, SaaS and Business Sites."
-                paragraph="The main ‘thrust’ is to focus on educating attendees on how to best protect highly vulnerable business applications with interactive panel discussions and roundtables."
+                title="The KC Lifesteal SMP."
+                paragraph="The KC Lifesteal SMP Is a Minecraft Server by KC Gaming 12. This server is about trying to survive, and if you lose all of your lives your Banned untill next season!"
                 mb="44px"
               />
 
@@ -35,15 +35,15 @@ const AboutSectionOne = () => {
               >
                 <div className="mx-[-12px] flex flex-wrap">
                   <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Premium quality" />
-                    <List text="Tailwind CSS" />
-                    <List text="Use for lifetime" />
+                    <List text="PVP" />
+                    <List text="Building" />
+                    <List text="Survival" />
                   </div>
 
                   <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Next.js" />
-                    <List text="Rich documentation" />
-                    <List text="Developer friendly" />
+                    <List text="Minecraft" />
+                    <List text="1.17-1.21.*" />
+                    <List text="Spawn Protection" />
                   </div>
                 </div>
               </div>
